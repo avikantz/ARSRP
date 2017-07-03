5d7cce58366f34719fc82d05a640208651a0be0d
avikantz <avikantsainidbz@gmail.com>
2017-07-03 22:04:19 +0530

    Some minor changes

--- ARSRP/ViewController.swift
+++ ARSRP/ViewController.swift
@@ -55,15 +55,16 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
-		let scene = SCNScene()
-		sceneView.scene = scene
-		let configuration = ARWorldTrackingSessionConfiguration()
-		configuration.planeDetection = .horizontal
-		sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+		var configuration = ARSessionConfiguration()
+		if ARWorldTrackingSessionConfiguration.isSupported {
+			configuration = ARWorldTrackingSessionConfiguration()
+		}
+		sceneView.session.run(configuration)
 	}
 	
 	override func viewWillDisappear(_ animated: Bool) {
 		sceneView.session.pause()
+//		sceneView.pause(self)
 	}
 	
 	func prepareGame() {
@@ -164,12 +165,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
 	
 	func sessionWasInterrupted(_ session: ARSession) {
 		// Inform the user that the session has been interrupted, for example, by presenting an overlay
+		print("Session was interrupted")
 		
 	}
 	
 	func sessionInterruptionEnded(_ session: ARSession) {
 		// Reset tracking and/or remove existing anchors if consistent tracking is required
-		
+		print("Session ended")
 	}
 	
 	// MARK: - Actions
